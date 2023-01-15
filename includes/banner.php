6f4fd6f615dcf9030163fb8c796ec97bff0b7d41
Sankalpa75 <bhattaraisankalpa1@gmail.com>
2023-01-15 22:36:43 +0545

    mock-up design

---
+++ includes/banner.php
@@ -0,0 +1,50 @@
+
+<div id="owl-demo" class="owl-carousel owl-theme">
+
+  <div class="item">
+    <div class="banner-content">
+      <img src="images/b1.jpg">
+      
+    </div>
+  </div>
+
+   <div class="item">
+    <div class="banner-content">
+      <img src="images/b2.jpg">
+      
+    </div>
+  </div>
+
+   <div class="item">
+    <div class="banner-content">
+      <img src="images/b3.jpg">
+      
+    </div>
+  </div>
+
+   <div class="item">
+    <div class="banner-content">
+      <img src="images/b4.jpg">
+      
+    </div>
+  </div>
+
+   <div class="item">
+    <div class="banner-content">
+      <img src="images/b5.jpg">
+      
+    </div>
+  </div>
+
+
+
+
+  
+
+</div>
+
+
+
+
+
+
